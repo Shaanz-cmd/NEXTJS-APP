b15586426ef696c46ab3ef539ea25cff6d786b6a
Shaan <shanali.ozhorik@Gmail.com>
2024-09-03 06:00:04 +0500

    first Commit

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
-export const signInSchema = z.object({
-    acceptMessages: z.boolean()
+export const messageSchema = z.object({
+    content:
+        z.string()
+            .min(10, { message: "content must be atleast 10 characters" })
+            .max(300, { message: "Max content length should not exceed 300 chars" })
 })
